2f10e76cf0766a7d7c7b502d7fdb13f0d5b2f36f
ninastjohn <ninastjohn7@gmail.com>
2026-02-21 11:34:32 -0800

    robot for nba rough

--- simulation/include/robot.hpp
+++ simulation/include/robot.hpp
@@ -1,1 +1,45 @@
 # pragma once
+#include <vector>
+#include <utility>
+
+
+class Robot{
+    public:
+        using Pos = std::pair<int,int>; // bot loc
+        Robot(int x, int y);
+
+        enum class Action {
+            Stay,
+            Left,
+            Right,
+            Up,
+            Down
+            /*
+            Later:
+            Open, Close, Pickup, Drop...
+            
+            */
+        }
+
+        Robot(int x, int y) : pos_{x,y} {}
+        virtual ~Robot() = default;         // allows dynamic dispatch
+
+        Pos position();
+        void set_position(int, int);
+
+        virtual cons std::vector<Action> & actions() const = 0;
+
+    protected:
+        Pos pos_; 
+
+}
+
+// Robot classes, using inheritance
+class Turtlebot: public Robot{
+    public:
+        using Robot::Robot;
+        const std::vector<Action>^& actions() const override;
+
+    private:
+        static const std::vector<Action> kActions_;
+}
--- simulation/src/robot.cpp
+++ simulation/src/robot.cpp
@@ -5,3 +5,27 @@
     can take
     - hopefully can be inflatted to add more features
 */
+
+#include "robot.hpp"
+
+// public returns
+Pos position() const {
+    return pos_; 
+}
+void set_position(int x, int y) { 
+    pos_ = {x,y};
+}
+
+// defining the turtlebot action set (static storage)
+
+const std::vector<Robot::Action> Turtlebot::kActions_ = {
+    Robot::Action::Stay,
+    Robot::Action::Left,
+    Robot::Action::Right,
+    Robot::Action::Up,
+    Robot::Action::Down
+}
+
+const std::vector<Robot::Actions>& Turtlebot::actions{} const {
+    return kActions_;
+}
